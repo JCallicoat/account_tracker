b53895cd1fbccc9f2021c0120426238e762c707e
Jordan Callicoat <jordan.callicoat@rackspace.com>
2012-10-07 18:05:38 -0500

    Implement search; css edits; pep8

--- tracker.py
+++ tracker.py
@@ -11,7 +11,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from contextlib import closing
 
 
-__version__ = 0.3
+__version__ = 0.5
 
 DATABASE = 'tracker.db'
 DEBUG = True
@@ -67,7 +67,8 @@ def teardown_request(exception):
 def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    entries = query_db('select * from customers where deleted=0 order by id asc')
+    entries = query_db('''select * from customers where deleted=0
+                          order by id asc''')
     if request.content_type == 'application/json':
         return json.dumps(entries)
     else:
@@ -78,7 +79,8 @@ def show_entries():
 def show_account(account_id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    account = query_db('select * from customers where deleted=0 and id=?', (account_id,), one=True)
+    account = query_db('select * from customers where deleted=0 and id=?',
+                       (account_id,), one=True)
     if request.content_type == 'application/json':
         if not account:
             return '{"Error": "Account not found"}', 404
@@ -88,8 +90,10 @@ def show_account(account_id):
         if not account:
             return render_template('show_account.html', account=account), 404
         else:
-            account['created_at'] = time.ctime(account['created_at']) + ' (UTC)'
-            account['updated_at'] = time.ctime(account['updated_at']) + ' (UTC)'
+            account['created_at'] = (time.ctime(account['created_at'])
+                                    + ' (UTC)')
+            account['updated_at'] = (time.ctime(account['updated_at'])
+                                    + ' (UTC)')
             return render_template('show_account.html', account=account, md=md)
 
 
@@ -98,26 +102,28 @@ def add_account():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     if request.method == 'GET':
-        return render_template('add_account.html', account=account)
+        return render_template('add_account.html')
     else:
         timestamp = time.time()
         g.db.execute('''insert into customers
-            (account_name, account_number, controller_ip, controller_id, admin_user, admin_pass, notes,
-             created_at, updated_at, deleted)
+            (account_name, account_number, controller_ip, controller_id,
+             admin_user, admin_pass, notes, created_at, updated_at, deleted)
             values (?, ?, ?, ?, ?, ?, ?, ?, ?, 0)''',
-            (request.form['account_name'], request.form['account_number'], request.form['controller_ip'],
-             request.form['controller_id'], request.form['admin_user'], request.form['admin_pass'],
+            (request.form['account_name'], request.form['account_number'],
+             request.form['controller_ip'], request.form['controller_id'],
+             request.form['admin_user'], request.form['admin_pass'],
              request.form['notes'], timestamp, timestamp))
         g.db.commit()
         flash('New account was successfully posted')
         return redirect(url_for('show_entries'))
 
 
-@app.route('/edit_account/<int:account_id>', methods=['GET', 'POST'])
+@app.route('/<int:account_id>/edit', methods=['GET', 'POST'])
 def edit_account(account_id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    account = query_db('select * from customers where deleted=0 and id=?', (account_id,), one=True)
+    account = query_db('select * from customers where deleted=0 and id=?',
+                       (account_id,), one=True)
     if request.method == 'GET':
         if request.content_type == 'application/json':
             if not account:
@@ -135,22 +141,42 @@ def edit_account(account_id):
         else:
             timestamp = time.time()
             g.db.execute('''insert or replace into customers
-                (id, account_name, account_number, controller_ip, controller_id, admin_user, admin_pass, notes,
-                 created_at, updated_at, deleted)
+                (id, account_name, account_number, controller_ip,
+                 controller_id, admin_user, admin_pass, notes, created_at,
+                 updated_at, deleted)
                 values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)''',
-                (account_id, request.form['account_name'], request.form['account_number'],
-                 request.form['controller_ip'], request.form['controller_id'], request.form['admin_user'],
-                 request.form['admin_pass'], request.form['notes'], account['created_at'], timestamp))
+                (account_id, request.form['account_name'],
+                 request.form['account_number'], request.form['controller_ip'],
+                 request.form['controller_id'], request.form['admin_user'],
+                 request.form['admin_pass'], request.form['notes'],
+                 account['created_at'], timestamp))
             g.db.commit()
             flash('Account was successfully edited')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('show_account', account_id=account_id))
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('search.html')
+    if request.method == 'GET':
+        return render_template('search.html')
+    else:
+        search_by = request.form.get('search_by')
+        search_text = request.form.get('search_text')
+        if request.content_type == 'application/json':
+            search_by = request.json.get('search_by')
+            search_text = request.json.get('search_text')
+        print(search_by, search_text)
+        search_regexp = re.compile(search_text)
+        entries = query_db('''select * from customers where deleted=0
+                              order by id asc''')
+        entries = [entry for entry in entries
+                   if search_regexp.search(str(entry[search_by]))]
+        if request.content_type == 'application/json':
+            return json.dumps(entries)
+        else:
+            return render_template('show_entries.html', entries=entries)
 
 
 def _format_error(request, error):
@@ -172,19 +198,20 @@ def login():
                 password = auth.get('password')
             else:
                 return _format_error(request, 'Invalid JSON')
-        
+
         if username != app.config['USERNAME']:
             return _format_error(request, 'Invalid username')
-        
+
         elif password != app.config['PASSWORD']:
             return _format_error(request, 'Invalid password')
-        
+
         else:
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
 
     return render_template('login.html', error=None, hide_login=True), 401
+
 
 @app.route('/logout')
 def logout():
